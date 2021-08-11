1f391f1da1983bf8b72dd7ba2c9a74198ba2cce6
Brittany Houtz <brittany@stackery.io>
2021-08-11 16:24:20 -0500

    initial commit with project scaffolding

---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = require("react");
+var react_dom_1 = require("react-dom");
+var App_1 = require("./components/App");
+react_dom_1.render(react_1.default.createElement(App_1.default, null), document.getElementById('root'));
